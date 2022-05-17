7ebdf025ec82ee60c1a4915876ea2d50c010e3af
nth2k <hiepnthe140416@fpt.edu.vn>
2022-05-17 15:30:55 +0700

    vue2

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input class="btn btn-primary" value="hello" type="button"/>
   </div>
 </template>
 
