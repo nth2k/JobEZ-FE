5b388e4170f6bcc3cef941ede8bf2cd6c7a1f90c
nth2k <hiepnthe140416@fpt.edu.vn>
2022-05-20 22:48:30 +0700

    5 screens layout

---
+++ src/views/ChooseCVType.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <TopHeaderComponent />
+    <div class="row">
+      <ChooseAccount />
+      <div class="col-6" style="padding-left: 100px; padding-top: 100px">
+        <div class="back-page">
+          <svg
+            width="24"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path
+              d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"
+            /></svg
+          ><span> Back</span>
+        </div>
+        <p class="mt-2" style="font-weight: bold">
+          Step 2: Complete the cv: You choose one of the following two ways to
+          complete the application
+        </p>
+        <div class="row mt-5 mb-5">
+          <div class="cv-online">
+            <img
+              src="https://media.gcflearnfree.org/content/55e0730c7dd48174331f5164_01_17_2014/whatisacomputer_desktop_computers.jpg"
+              alt=""
+            />
+            <span style="font-weight: bold">Use Online form</span>
+          </div>
+          <div class="cv-from-pc">
+            <img
+              src="https://png.pngtree.com/png-vector/20191030/ourlarge/pngtree-adaptive-computer-mobile-pc-responsive-icon-for-web-png-image_1927634.jpg"
+              alt=""
+            />
+            <span style="font-weight: bold">Upload CV from PC</span>
+          </div>
+        </div>
+        <div class="guide mt-5 p-2">
+          <span style="font-weight: bold; color: red">Tutorial:</span>
+          <p>
+            <span style="font-weight: bold"
+              >Create a profile with an online declaration form:</span
+            >
+            you create all your information for employers to have a basis for
+            selection. The more detailed information your job opportunities
+            increase
+          </p>
+          <p>
+            <span style="font-weight: bold"
+              >Upload your cv from your computer:</span
+            >
+            you already have your resume ready from your computer, you just need
+            to upload your cv and confirm your email to complete the
+            registration process.
+          </p>
+          <p style="font-weight: bold">
+            Both profiles are searchable by employers
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChooseAccount from "@/components/ChooseAccount.vue";
+import TopHeaderComponent from "@/components/TopHeaderComponent.vue";
+export default {
+  name: "ChooseCVType",
+  components: {
+    ChooseAccount,
+    TopHeaderComponent,
+  },
+};
+</script>
+
+<style scoped>
+.cv-online {
+  background-color: #ffefcf;
+  margin-left: 80px;
+  height: 150px;
+  width: 150px;
+  padding: 0;
+  border-radius: 10px;
+}
+
+.cv-online img {
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
+}
+
+.cv-online span {
+  display: none;
+}
+
+.cv-online:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  transition: all 0.8s;
+}
+
+.cv-online:hover > span {
+  display: inline-block;
+}
+
+.cv-from-pc {
+  background-color: #e1f5ff;
+  margin-left: 50px;
+  height: 150px;
+  width: 150px;
+  border-radius: 10px;
+}
+
+.cv-from-pc img {
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
+}
+
+.cv-from-pc span {
+  display: none;
+}
+
+.cv-from-pc:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  transition: all 0.8s;
+}
+
+.cv-from-pc:hover > span {
+  display: inline-block;
+}
+
+.guide {
+  width: 577px;
+  height: 247px;
+  left: 736px;
+  top: 490px;
+  background: #fbfbff;
+  border: 1.5px dashed #b4c6e7;
+  border-radius: 20px;
+}
+</style>
---
+++ src/views/OnlineCVForm.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <h2 class="text-center mt-3">Candidate Profile</h2>
+    <form action="" method="post">
+      <div class="row mt-5">
+        <div class="col-3"></div>
+        <div class="col-3 left-form">
+          <span>Birthdate<span style="color: red">*</span></span
+          ><br />
+          <input type="text" placeholder="Enter dd/MM/yyyy" /><br />
+          <span>Gender<span style="color: red">*</span></span
+          ><br />
+          <select name="gender" id="">
+            <option value="1">Male</option>
+            <option value="0">Female</option></select
+          ><br />
+          <span>Marital status<span style="color: red">*</span></span
+          ><br />
+          <select name="marital" id="">
+            <option value="0">Single</option>
+            <option value="1">Married</option></select
+          ><br />
+          <span>University<span style="color: red">*</span></span
+          ><br />
+          <input type="text" placeholder="Enter university name" /><br />
+          <span>Rating<span style="color: red">*</span></span
+          ><br />
+          <select name="rating" id="">
+            <option value="0">Excellent certificate</option>
+            <option value="1">Good certificate</option>
+            <option value="2">Average certificate</option></select
+          ><br />
+          <span>Years of experience<span style="color: red">*</span></span
+          ><br />
+          <select name="experience" id="">
+            <option value="1">1 year</option>
+            <option value="2">2 years</option>
+            <option value="3">3 years</option>
+            <option value="4">4 years</option>
+            <option value="5">5 years</option></select
+          ><br />
+          <span>Salary<span style="color: red">*</span></span
+          ><br />
+          <select name="salary" id="">
+            <option value="0">Less than 5M</option>
+            <option value="1">5M to 10M</option>
+            <option value="2">10M to 20M</option>
+            <option value="3">20M to 30M</option>
+            <option value="4">Greater than 30M</option></select
+          ><br />
+          <span>Working form<span style="color: red">*</span></span
+          ><br />
+          <select name="workingForm" id="">
+            <option value="0">On site</option>
+            <option value="1">Remote</option></select
+          ><br />
+        </div>
+        <div class="col-5 right-form">
+          <span>Career goals<span style="color: red">*</span></span
+          ><br />
+          <span style="font-style: italic; color: #7a7a7a"
+            >(can add multiple goals or add description)</span
+          ><br />
+          <input type="checkbox" name="" id="" />Looking for a stable and
+          long-term job<br />
+          <input type="checkbox" name="" id="" />Looking for a job with good
+          advancement opportunities<br />
+          <input type="checkbox" name="" id="" />Looking for a job with good
+          salary<br />
+          <input type="checkbox" name="" id="" />Looking for a place with a good
+          opportunity to dedicate myself<br />
+          <input type="checkbox" name="" id="" />Desire to work in a
+          professional environment<br />
+          <span style="font-style: italic" class="ml-5"
+            >Description of career goals (if any):</span
+          ><br />
+          <textarea class="mt-3" rows="5" cols="50"></textarea><br />
+          <span>Personal skills<span style="color: red">*</span></span
+          ><br />
+          <span style="font-style: italic; color: #7a7a7a"
+            >(can add multiple skills or add description)</span
+          ><br />
+          <input type="checkbox" name="" id="" />Know how to listen and receive
+          ideas<br />
+          <input type="checkbox" name="" id="" />Hardworking, diligent to learn
+          about problems in work and life<br />
+          <input type="checkbox" name="" id="" />Sense of responsibility at
+          worky<br />
+          <input type="checkbox" name="" id="" />Withstand pressure at work<br />
+          <input type="checkbox" name="" id="" />Have fun with colleagues<br />
+          <span style="font-style: italic" class="ml-5"
+            >Description of career skills (if any):</span
+          ><br />
+          <textarea class="mt-3" rows="5" cols="50"></textarea><br />
+        </div>
+        <div class="col-1"></div>
+        <span class="mt-5 w-100 text-center"
+          >By clicking the register button, you agree to JobEZ's usage
+          agreement</span
+        >
+        <button type="submit" class="btn mt-5 mb-5 btn-regist">Regist</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+export default {
+  name: "OnlineCVForm",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style scoped>
+.left-form input {
+  width: 80%;
+  border-radius: 5px;
+  border: 1px solid #bdbdbd;
+  height: 40px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.left-form select {
+  width: 80%;
+  border-radius: 5px;
+  border: 1px solid #bdbdbd;
+  height: 40px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.right-form input {
+  margin-top: 20px;
+  width: 15px;
+  height: 15px;
+  margin-right: 20px;
+}
+
+.btn-regist {
+  display: block;
+  margin: 0 auto;
+  background-color: #4c5bd4;
+  color: white;
+  width: 319px;
+  height: 46px;
+  border-radius: 25px;
+}
+</style>
