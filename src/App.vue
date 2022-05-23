e42c5cb1fa5905a957addea59eb3cd2348f7fe1c
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-05-23 20:11:55 +0700

    add JobSaved.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <JobSaved />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import JobSaved from './views/JobSaved.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JobSaved
   }
 }
 </script>
---
+++ src/views/JobSaved.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>this is Jobsaved page </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
