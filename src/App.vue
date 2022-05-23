84ed2072b22572ef32f4887a93781428479c6e03
codehunter <nguyenthetoan1606@gmail.com>
2022-05-23 10:17:46 +0700

    update man hinh ung vien

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <div>
-    <div class="container-fluid"></div>
-    <Header />
-    <!-- <Banner /> -->
-    <SearchHeader />
-    <Navigator />
-    <BoxTiki />
-    <JobContainer />
+  <div id="app">
+    <LandingPage />
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Header from "./components/views/candidate_views/Header.vue";
-// import Banner from "./components/views/candidate_views/Banner.vue";
-import SearchHeader from "./components/views/candidate_views/SearchHeader.vue";
-import Navigator from "./components/views/candidate_views/Navigator.vue";
->>>>>>> d4a7aeb900e8370d197710c00d0c02fe265f79ac
+import LandingPage from "./views/LandingPage.vue";
 
 export default {
-    Header,
-    BoxTiki,
->>>>>>> d4a7aeb900e8370d197710c00d0c02fe265f79ac
+  name: "App",
+  components: {
+    LandingPage,
+  },
+};
+</script>
 
 <style>
 </style>
