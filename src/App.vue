9b2075dafd905606d5665c39527511f0b0d774ab
codehunter <nguyenthetoan1606@gmail.com>
2022-05-25 20:05:33 +0700

    tao man hinh chi tiet cong viec

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <Header />
-    <SearchHeader />
-    <Navigator />
-    <JobContainer />
+    <CandidateProfile />
   </div>
 </template>
 
 <script>
-import Header from "./components/views/recruiter_views/Header.vue";
-import SearchHeader from "./components/views/candidate_views/SearchHeader.vue";
-import Navigator from "./components/views/candidate_views/Navigator.vue";
-import JobContainer from "./components/views/candidate_views/job_details/JobContainer.vue";
+import CandidateProfile from "./components/views/candidate_views/CandidateProfile.vue";
 
 export default {
   name: "App",
   components: {
-    Header,
-    SearchHeader,
-    Navigator,
-    JobContainer,
+    CandidateProfile,
   },
 };
 </script>
---
+++ src/components/views/recruiter_views/RecruiterSummary.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="container pt-5 summary-container">
+      <div class="row">
+        <div class="col-12 col-xl-8">
+          <RecruiterDescription />
+        </div>
+        <div class="col-12 col-xl-4">
+          <RecruiterAddress />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RecruiterAddress from "./recruiter_summary/RecruiterAddress.vue";
+import RecruiterDescription from "./recruiter_summary/RecruiterDescription.vue";
+
+export default {
+  name: "RecruiterSummary",
+  components: {
+    RecruiterAddress,
+    RecruiterDescription,
+  },
+};
+</script>
+
+<style scoped>
+.summary-container {
+  border-bottom: 0.5px solid gray;
+}
+</style>
