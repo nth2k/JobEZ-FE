06d6f32f2674cdbd36c8120145d2ddac1c73b1d3
codehunter <nguyenthetoan1606@gmail.com>
2022-06-06 20:31:49 +0700

    add CV_01

---
+++ src/views/ToanNT16/cv-detail/CV_01.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid">
+    <div class="d-flex"><div class=""></div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CV-01",
+};
+</script>
+
+<style>
+</style>
