bc70be0ca5cf21b409e9f033463208276c543178
codehunter <nguyenthetoan1606@gmail.com>
2022-06-11 11:18:00 +0700

    add CV02

---
+++ src/views/ToanNT16/cv-detail/CV_02/CvMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-12"><CvTop /></div>
+    <div class="col-12"><CvContent /></div>
+  </div>
+</template>
+
+<script>
+import CvTop from "./CvTop.vue";
+import CvContent from "./CvContent.vue";
+
+export default {
+  name: "CvMain",
+  components: {
+    CvTop,
+    CvContent,
+  },
+};
+</script>
+
+<style>
+</style>
