43557c12ce04d1e8ce82caa20f7da9394dde446f
nth2k <hiepnthe140416@fpt.edu.vn>
2022-06-15 16:55:08 +0700

    test api

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
-  <!-- <CandidateProfile /> -->
-  <!-- <JobDetails /> -->
-  <!-- <RecruiterProfile /> -->
-  <CV_02 />
+  <div>
+    <!-- <router-view></router-view> -->
+    <TestApi />
+  </div>
 </template>
 
 <script>
-// import CandidateProfile from "./components/views/candidate_views/CandidateProfile.vue";
-// import JobDetails from "./components/views/recruiter_views/JobDetails.vue";
-// import Header from "./components/views/Header.vue";
-// import RecruiterProfile from "./components/views/RecruiterProfile.vue";
-// import ProfileHeader from "./components/views/candidate_views/candidate_management/candidate_management_profile/ProfileHeader.vue";
-// import ListOfCv from "./views/ToanNT16/cv_menu/CVs.vue";
-import CV_02 from "./views/ToanNT16/cv-detail/CV_02.vue";
-
+import TestApi from "@/views/HiepViews/TestApi.vue"
 export default {
   name: "App",
-  components: {
-    CV_02,
-  },
+  components: {TestApi}, 
 };
 </script>
 
---
+++ src/services/EmployeeService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const Base_URL = "http://localhost:8000";
+
+class EmployeeService {
+  getEmployee() {
+    return axios.get(Base_URL + "/employees");
+  }
+
+  addEmployee(employee) {
+    return axios.post(Base_URL + "/employees" , employee)
+  }
+
+  deleteEmployee(id){
+    return axios.delete(Base_URL + "/employees/" + id)
+  }
+}
+
+export default new EmployeeService();
