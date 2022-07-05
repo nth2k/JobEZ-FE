9bbdab199ce388170807f4a79b9b185af4985206
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-07-05 19:20:43 +0700

    update language certificate service

--- src/services/LanguageCertificateService.js
+++ src/services/LanguageCertificateService.js
@@ -4,13 +4,19 @@ const Base_URL = "http://localhost:8080";
 
 class LanguageCertificateService{
     getLanguage(){
-        return axios.get(Base_URL + "/language/1");
+        return axios.get(Base_URL + "/language/user/1");
+    }
+    findLanguage(id){
+        return axios.get(Base_URL + "/language/editlanguage/" + id);
     }
     addLanguage(language){
-        return axios.post(Base_URL + "/language/add", language)
+        return axios.post(Base_URL + "/language/add", language);
+    }
+    updateLanguage(language, id){
+        return axios.post(Base_URL+ "/update/" + id, language);
     }
     deleteLanguage(id){
-        return axios.delete(Base_URL + "/delete/" + id)
+        return axios.delete(Base_URL + "/delete/" + id);
     }
 }
 
