2a926b4920beccbab7e64bb73da99d1a9c7c6bf7
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-07-07 15:06:15 +0700

    adad

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
     <!-- <TestApi /> -->
     <!-- <ViewNgoaiNguTinHoc /> -->
-    <ViewKinhNghiemLamViec />
-    <SavedJob />
+    <!-- <EditNgoainguTinhoc /> -->
+    <!-- <ViewKinhNghiemLamViec /> -->
+    <!-- <SavedJob /> -->
   </div>
 </template>
 
 <script>
-import ViewKinhNghiemLamViec from './views/TuanNV/ViewKinhNghiemLamViec.vue';
+// import ViewKinhNghiemLamViec from './views/TuanNV/ViewKinhNghiemLamViec.vue';
 // import ViewNgoaiNguTinHoc from './views/TuanNV/ViewNgoaiNguTinHoc.vue';
+// import EditNgoainguTinhoc from './views/TuanNV/EditNgoainguTinhoc.vue';
 // import SavedJob from './views/TuanNV/SavedJob.vue';
 // import TestApi from "@/views/HiepViews/TestApi.vue"
 export default {
   name: "App",
   components: {
-    // ViewNgoaiNguTinHoc, 
-    // SavedJob,
-    ViewKinhNghiemLamViec
+    // ViewNgoaiNguTinHoc,
+    // EditNgoainguTinhoc
 },
 };
 </script>
