1619c2f17924b3892a1404167422bf42e240f681
nth2k <hiepnthe140416@fpt.edu.vn>
2022-07-16 15:30:11 +0700

    province district api

--- src/services/RecruiterRegisterService.js
+++ src/services/RecruiterRegisterService.js
@@ -6,9 +6,6 @@ const RecruiterRegisterService = {
   addRecruiter(recruiter) {
     return axios.post(Base_URL + "/recruiterRegister/", recruiter);
   },
-  getProvince() {
-    return axios.get("https://provinces.open-api.vn/api/?depth=3");
-  },
   updateRecruiter(id, recruiter){
     return axios.put(Base_URL + "/recruiterOnlineCVForm/" + id, recruiter);
   }
