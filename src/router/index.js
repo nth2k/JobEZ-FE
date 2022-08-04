37072ae4aa62d31681ae88b81c21084b69d914b8
nth2k <hiepnthe140416@fpt.edu.vn>
2022-08-04 21:35:10 +0700

    add tuannv

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,13 @@ import RecruiterOnlineCVForm from '@/views/HiepViews/RecruiterOnlineCVForm.vue'
 import JobDetailsNoLogin from '@/components/ToanComponents/JobDetailsNoLogin.vue'
 import SearchJob from '@/views/HiepViews/SearchJob.vue'
 import SearchCandidate from '@/views/HiepViews/SearchCandidate.vue'
+// import AddLanguageCertificate from '@/views/TuanNV/AddLanguageCertificate.vue'
+// import EditLanguageCertificate from '@/views/TuanNV/EditLanguageCertificate.vue'
+// import ViewLanguageCertificate from '@/views/TuanNV/ViewLanguageCertificate.vue'
+// import SavedJob from '@/views/TuanNV/SavedJob.vue'
+// import AppliedJob from '@/views/TuanNV/AppliedJob.vue'
+// import ViewWorkExperience from '@/views/TuanNV/ViewWorkExperience.vue'
+// import AddWorkExperience from '@/views/TuanNV/AddWorkExperience.vue'
 
 Vue.use(VueRouter)
 
@@ -25,42 +32,42 @@ export const routes = [
         path: '/candidateLogin',
         name: 'CandidateLogin',
         component: CandidateLogin
-    },  
+    },
     {
         path: '/candidateRegister',
         name: 'CandidateRegister',
         component: CandidateRegister
-    },  
+    },
     {
         path: '/recruiterLogin',
         name: 'RecruiterLogin',
         component: RecruiterLogin
-    }, 
+    },
     {
         path: '/recruiterRegister',
         name: 'RecruiterRegister',
         component: RecruiterRegister
-    }, 
+    },
     {
         path: '/chooseCVType/:id',
         name: 'ChooseCVType',
         component: ChooseCVType
-    }, 
+    },
     {
         path: '/candidateOnlineCVForm/:id',
         name: 'CandidateOnlineCVForm',
         component: CandidateOnlineCVForm
-    }, 
+    },
     {
         path: '/cvFromPCForm/:id',
         name: 'CVFromPCForm',
         component: CVFromPCForm
-    }, 
+    },
     {
         path: '/recruiterOnlineCVForm/:id',
         name: 'RecruiterOnlineCVForm',
         component: RecruiterOnlineCVForm
-    }, 
+    },
     {
         path: '/job-detail/:id',
         name: 'JobDetailsNoLogin',
@@ -75,6 +82,41 @@ export const routes = [
         path: '/searchCandidate',
         name: 'SearchCandidate',
         component: SearchCandidate
+    },
+    {
+        path: '/addLanguage',
+        name: 'AddLanguageCertificate',
+        component: () => import("@/views/TuanNV/AddLanguageCertificate.vue")
+    }, 
+    {
+        path: '/editLanguage/:id',
+        name: 'EditLanguageCertificate',
+        component: () => import("@/views/TuanNV/EditLanguageCertificate.vue")
+    }, 
+    {
+        path: '/language',
+        name: 'ViewLanguageCertificate',
+        component: () => import("@/views/TuanNV/ViewLanguageCertificate.vue")
+    }, 
+    {
+        path: '/savedjobs',
+        name: 'SavedJob',
+        component: () => import("@/views/TuanNV/SavedJob.vue")
+    }, 
+    {
+        path: '/appliedjob',
+        name: 'AppliedJob',
+        component: () => import("@/views/TuanNV/AppliedJob.vue")
+    }, 
+    {
+        path: '/workexp',
+        name: 'ViewWorkExperience',
+        component: () => import("@/views/TuanNV/ViewWorkExperience.vue")
+    }, 
+    {
+        path: '/addworkexp',
+        name: 'AddWorkExperience',
+        component:() => import("@/views/TuanNV/AddWorkExperience.vue")
     }
     // {
     //     path: '/profileCandidate',
