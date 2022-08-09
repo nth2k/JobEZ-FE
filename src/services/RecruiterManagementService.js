11b31b6064b287e7819db04c5b64bdc8cc5df3b0
nth2k <hiepnthe140416@fpt.edu.vn>
2022-08-09 21:13:39 +0700

    recruiter management

--- src/services/RecruiterManagementService.js
+++ src/services/RecruiterManagementService.js
@@ -6,6 +6,18 @@ class RecruiterManagementService {
   addPosting(post) {
     return axios.post(Base_URL + "/posting/addPosting", post);
   }
+  getPostedPosting(userId) {
+    return axios.get(Base_URL + "/posting/getPosting/" + userId);
+  }
+  updatePosting(postingId, posting){
+    return axios.put(Base_URL + "/posting/updatePosting/" + postingId, posting);
+  }
+  getPostingById(postingId){
+    return axios.get(Base_URL + "/posting/updatePosting/" + postingId);
+  }
+  deletePostingById(postingId){
+    return axios.delete(Base_URL + "/posting/updatePosting/" + postingId);
+  }
 }
 
 export default new RecruiterManagementService();
