b77e57b32cd4d12b3ac539f0aa13cc1b15346f9a
codehunter <nguyenthetoan1606@gmail.com>
2022-08-15 21:02:11 +0700

    update save job and applied job

--- src/services/SavedJobService.js
+++ src/services/SavedJobService.js
@@ -2,13 +2,22 @@ import axios from "axios";
 
 const Base_URL = "http://localhost:8081";
 
-class SavedJobService{
-    getSavedJobs(id){
-        return axios.get(Base_URL + "/savedjob/" + id);
-    }
-    deleteSavedJob(savedjobkey){
-        return axios.post(Base_URL + "/savedjob/remove", savedjobkey);
-    }
+class SavedJobService {
+  getSavedJobs(id) {
+    return axios.get(Base_URL + "/savedjob/" + id);
+  }
+  deleteSavedJob(savedjobkey) {
+    return axios.post(Base_URL + "/savedjob/remove", savedjobkey);
+  }
+  async addSavedJob(commit, email, postingId) {
+    const body = {
+      email: email,
+      posting_id: postingId,
+    };
+    await axios.post(Base_URL + "/savedjob/add", body).then((response) => {
+      commit("SET_SAVED_JOB", response.data);
+    });
+  }
 }
 
 export default new SavedJobService();
